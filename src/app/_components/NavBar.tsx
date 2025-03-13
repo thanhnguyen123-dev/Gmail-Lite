20dc10042639a8667b36b73975387d2d2990e638
Thanh Nguyen <lethanh300504@gmail.com>
2025-03-13 23:32:55 +1100

    add onclick to thread id page

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import React from 'react';
 import { SiGmail } from "react-icons/si";
 import SearchBar from './SearchBar';
 import Avatar from './Avatar';
-
+import { useRouter } from 'next/navigation';
 const NavBar = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/threads');
+  }
+
   return (
     <div className="sticky top-0 flex justify-between items-center w-full border-b border-slate-200 py-2 px-8">
       <div className="flex items-center gap-2">
-        <SiGmail size={24} />
+        <SiGmail role="button" size={24} onClick={handleClick} />
         <h1 className="text-xl font-bold">Gmail Lite</h1>
       </div>
       <SearchBar />
